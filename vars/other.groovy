2fd40e4523f88a2d078903691e1756552771846b
Petr Balogh <pbalogh@redhat.com>
2018-04-05 14:10:20 +0200

    Add function for get labels of locked resource
    Also added function to check if env is HE.

--- vars/other.groovy
+++ vars/other.groovy
@@ -1,7 +1,18 @@
-def get_locked_ge_name() {
-  return org.jenkins.plugins.lockableresources.LockableResourcesManager.class.get().getResourcesFromBuild(currentBuild.getRawBuild())[0].getName()
+def get_locked_resources() {
+  return org.jenkins.plugins.lockableresources.LockableResourcesManager.class.get().getResourcesFromBuild(currentBuild.getRawBuild())
 }
 
+def get_locked_ge_name() {
+  get_locked_resources()[0].getName()
+}
+
+def get_locked_ge_labels() {
+  get_locked_resources()[0].getLabels()
+}
+
+def is_he_env() {
+  get_locked_ge_labels().contains("he-env")
+}
 
 def get_username() {
   def cause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
