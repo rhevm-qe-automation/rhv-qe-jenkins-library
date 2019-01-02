9c33b384cd6f46558dec38c1fc73588a445676a2
dlaguoto <dlaguoto@redhat.com>
2019-01-02 15:54:20 +0200

    adding functions for sending emailnotifications when production job fails
    Related JIRA ticket:
      https://projects.engineering.redhat.com/browse/RHEVM-5322

--- vars/jobs.groovy
+++ vars/jobs.groovy
@@ -38,3 +38,16 @@ def build_job(name, parameters, is_stable)
   }
   return job
 }
+
+def build_and_notify(name, parameters, ignore_unstable=false){
+  def current_job = build job: name, parameters: parameters, propagate: false, wait: true
+  if (current_job.result != 'SUCCESS') {
+    if (env.NODE_NAME.contains('production')) {
+      if (ignore_unstable && current_job.result == 'UNSTABLE')
+        return
+
+      notification.notificator(env.BUILD_URL, get_job_url(current_job))
+    }
+    throw new Exception(other.parse_url(get_job_url(current_job), true) + " has TERMINATED with status " + current_job.result)
+  }
+}
