45fc36e00a3275c26b5b8bd501f12119db15b303
dlaguoto <dlaguoto@redhat.com>
2019-07-24 15:02:15 +0300

    revision of rvh-qe-jenkins-library
    * moving venv-ansible script from kobihk to rhevm-qe-automation repo
    * renaming the vars/ansible.groovy call to more specific: vars/v2v-ansible.groovy

--- vars/v2v_ansible.groovy
+++ vars/v2v_ansible.groovy
@@ -1,14 +1,11 @@
-// vars/ansible.groovy
+// vars/v2v_ansible.groovy
 def call(Map config = [:]) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    def toolPath = "venv-ansible"
     def playbook = config.get('playbook')
     def inventory = config.get('inventory', "jenkins/qe/v2v/inventory")
     def tags = config.get('tags', [])
     def extraVars = config.get('extraVars', [])
-    def gitUrl = config.get('gitUrl', 'https://github.com/kobihk/venv-ansible.git')
-    def gitBranch = config.get('gitBranch', 'add_req_master')
     def ansibleParams = ""
     if (tags) {
       ansibleParams += "--tags ${tags.join(',')}"
@@ -17,10 +14,9 @@ def call(Map config = [:]) {
       ansibleParams += " -e ${it}"
     }
     echo "All ansible params: ${ansibleParams}"
-    dir (toolPath) {
-      git url: "${gitUrl}", branch: "${gitBranch}"
+    dir ('venv-ansible') {
       ansiColor('xterm') {
-        sh "${WORKSPACE}/${toolPath}/venv-ansible ${inventory} ${playbook} $ansibleParams"
+        sh "${WORKSPACE}/venv-ansible/venv-ansible ${inventory} ${playbook} $ansibleParams"
       }
     }
 }
