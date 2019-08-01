5eb02b3b985e43c88f1e164062f7795b1c760333
dlaguoto <dlaguoto@redhat.com>
2019-08-01 17:31:11 +0300

    automation of adding info into "GE Executions" google doc sheet
    Related JIRA ticket:
     https://projects.engineering.redhat.com/browse/RHEVM-5311

--- vars/jobs.groovy
+++ vars/jobs.groovy
@@ -14,7 +14,7 @@ def get_job_url(job){
 def build_job(name, parameters, is_stable)
 {
   def job = null
-  job = build job: name, parameters: parameters, propagate: false
+  job = build job: name, parameters: parameters, propagate: false, wait: true
   if(is_stable)
   {
     if(job.getResult().contains("SUCCESS"))
@@ -39,14 +39,22 @@ def build_job(name, parameters, is_stable)
   return job
 }
 
-def build_and_notify(name, parameters, ignore_unstable=false){
-  def current_job = build job: name, parameters: parameters, propagate: false, wait: true
+def build_and_notify(name, parameters, cherry_pick, ignore_unstable=false){
+  def current_job = null
+  def cause = currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause).toString()
+  if (cause && cause.contains('production-testing-pipeline')) {
+    current_job = job_monitoring(ref: cherry_pick, job_parameters: parameters, name: name, update_status: null)
+  }
+  else {
+    current_job = build job: name, parameters: parameters, propagate: false, wait: true
+  }
+
   if (current_job.result != 'SUCCESS') {
     if (env.NODE_NAME.contains('production')) {
       if (ignore_unstable && current_job.result == 'UNSTABLE')
         return
 
-      notification.notificator(env.BUILD_URL, get_job_url(current_job))
+      //notification.notificator(env.BUILD_URL, get_job_url(current_job))
     }
     throw new Exception(other.parse_url(get_job_url(current_job), true) + " has TERMINATED with status " + current_job.result)
   }
